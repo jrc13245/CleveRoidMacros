d22019545b975661dfb26f44c13d47c598edf76b
Jrc13245 <grc2.grc2@gmail.com>
2025-08-07 13:59:43 -0400

    revert

--- Extensions/Mouseover/MouseoverTracker.lua
+++ Extensions/Mouseover/MouseoverTracker.lua
@@ -19,43 +19,10 @@ function Extension.OnUnitLeaveFrame()
 end
 
 function Extension.OnLoad()
-    if not CleveRoids.hasSuperwow or not SetMouseoverUnit then
-        return
-    end
-
-    -- This single script handles the OnEnter event for all unit frames.
-    local on_enter_script = function()
-        local unit = this:GetAttribute("unit")
-
-        -- Use the unit from the attribute if it's a valid, existing unit.
-        if unit and UnitExists(unit) then
-            SetMouseoverUnit(unit)
-        end
-    end
-
-    local on_leave_script = function()
-        SetMouseoverUnit()
-    end
-
-    -- Attach the unified scripts to all known unit frames, excluding the PlayerFrame.
-    TargetFrame:SetScript("OnEnter", on_enter_script)
-    TargetFrame:SetScript("OnLeave", on_leave_script)
-
-    for i = 1, 4 do
-        local partyFrame = _G["PartyMemberFrame"..i]
-        if partyFrame then
-            partyFrame:SetScript("OnEnter", on_enter_script)
-            partyFrame:SetScript("OnLeave", on_leave_script)
-        end
-    end
-
-    for i = 1, 40 do
-        local raidFrame = _G["RaidFrame"..i]
-        if raidFrame then
-            raidFrame:SetScript("OnEnter", on_enter_script)
-            raidFrame:SetScript("OnLeave", on_leave_script)
-        end
-    end
+    -- This frame is just for holding the event handlers. We'll attach the scripts to a temporary frame.
+    local tempFrame = CreateFrame("Frame")
+    tempFrame:SetScript("OnEnter", Extension.OnUnitEnterFrame)
+    tempFrame:SetScript("OnLeave", Extension.OnUnitLeaveFrame)
 end
 
 _G["CleveRoids"] = CleveRoids
